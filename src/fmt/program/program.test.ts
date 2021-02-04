bcd5f7bc12549461fa73351bbcdc82a18c010566
nizami <nizami.dev@gmail.com>
2021-02-05 00:15:11 +0300

    Update tsconfig.dist.json

--- src/fmt/program/program.test.ts
+++ src/fmt/program/program.test.ts
@@ -5,7 +5,5 @@ test('program', () => {
   const code = 'my-scope   / libname : UUID as U4  ,  Math\n2+2\n';
   const tree = parseProgram(code);
   const fmt = new ProgramFormatter(tree);
-  console.log(fmt.formattedCode());
-
   expect(fmt.formattedCode()).toBe('my-scope/libname: UUID as U4, Math\n2 + 2');
 });
