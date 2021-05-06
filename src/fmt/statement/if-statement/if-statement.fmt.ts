4ff834bbe88b76ee8e6884728af2743abf4d0225
nizami <nizami.dev@gmail.com>
2021-05-06 22:00:59 +0300

    Update If statement

--- src/fmt/statement/if-statement/if-statement.fmt.ts
+++ src/fmt/statement/if-statement/if-statement.fmt.ts
@@ -1,23 +1,10 @@
 import { IfStatementTree } from '@xon/ast';
-import { config } from '../../../formatter-config';
-import { getExpressionFormatter } from '../../expression/expression-helper';
-import { getStatementFormatter } from '../statement-helper';
 import { StatementFormatter } from '../statement.fmt';
 
 export class IfStatementFormatter extends StatementFormatter {
   tree: IfStatementTree;
 
   formattedCode(): string {
-    const items = this.tree.items.map((x) => {
-      const condition = getExpressionFormatter(x.condition).formattedCode();
-      const statements = x.statements
-        .map((z) => config.tab() + getStatementFormatter(z).formattedCode())
-        .join(config.newLine);
-      const breakStatements = config.breakIfStatement || x.statements.length > 1;
-      return `${x.keyword}${condition ? ` ${condition}` : ''}:${
-        breakStatements ? config.newLine + statements : ` ${statements.trim()}`
-      }`;
-    });
-    return items.join(config.newLine);
+    return '';
   }
 }
