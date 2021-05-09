8f2f6cd0b8f3a9e626313b8090de05e81b7d3c91
nizami <nizami.dev@gmail.com>
2021-05-09 23:01:16 +0300

    Update Program formatter

--- src/fmt/program/program.fmt.ts
+++ src/fmt/program/program.fmt.ts
@@ -2,6 +2,8 @@ import { ProgramTree } from '@xon/ast';
 import { config } from '../../formatter-config';
 import { BaseFormatter } from '../base.fmt';
 import { formatDefinitionTree } from '../definition/definition.fmt.helper';
+import { formatExtensionMethodTree } from '../extension-method/extension-method.fmt.helper';
+import { formatFunctionTree } from '../function/function.fmt.helper';
 import { formatStatementTree } from '../statement/statement.fmt.helper';
 
 export class ProgramFormatter extends BaseFormatter {
@@ -9,8 +11,12 @@ export class ProgramFormatter extends BaseFormatter {
 
   formattedCode(): string {
     const statements = this.tree.statements.map((x) => formatStatementTree(x)).join(config.nl);
+    const functions = this.tree.functions.map((x) => formatFunctionTree(x)).join(config.nl);
+    const extensionMethods = this.tree.extensionMethods
+      .map((x) => formatExtensionMethodTree(x))
+      .join(config.nl);
     const definitions = this.tree.definitions.map((x) => formatDefinitionTree(x)).join(config.nl);
 
-    return [statements, definitions].filter((x) => x).join(config.nl2);
+    return [statements, functions, extensionMethods, definitions].filter((x) => x).join(config.nl2);
   }
 }
