b69eeae1d0de67b194e6ae3c259439fa1780c602
nizami <nizami.dev@gmail.com>
2021-05-09 21:36:47 +0300

    Update code-formatter test

--- src/code-formatter.test.ts
+++ src/code-formatter.test.ts
@@ -2,5 +2,5 @@ import { formatCode } from './code-formatter';
 
 test('config', () => {
   const code = '12341234\n\n\n\nif   a:\n    1   + 1';
-  expect(formatCode(code)).toBe(`12_341_234\nif a:\n    1 + 1`);
+  expect(formatCode(code)).toBe(`12341234\nif a:\n    1 + 1`);
 });
